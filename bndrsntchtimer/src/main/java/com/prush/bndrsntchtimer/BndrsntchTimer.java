36bfbfaba8f3216ec8b726e346c80d7d2ec98743
iamporus <iamporus@gmail.com>
2019-06-14 17:18:38 +0530

    Initial commit.

---
+++ bndrsntchtimer/src/main/java/com/prush/bndrsntchtimer/BndrsntchTimer.java
@@ -0,0 +1,25 @@
+package com.prush.bndrsntchtimer;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+@SuppressWarnings( { "unused", "SpellCheckingInspection" } )
+public class BndrsntchTimer extends View
+{
+    public BndrsntchTimer( Context context )
+    {
+        super( context );
+    }
+
+    public BndrsntchTimer( Context context, @Nullable AttributeSet attrs )
+    {
+        super( context, attrs );
+    }
+
+    public BndrsntchTimer( Context context, @Nullable AttributeSet attrs, int defStyleAttr )
+    {
+        super( context, attrs, defStyleAttr );
+    }
+}
